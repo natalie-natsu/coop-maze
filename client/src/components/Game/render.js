12f05f67febe497f97f371da07ddb9c31d34bd10
Samy Laumonier <samy.laumonier@gmail.com>
2018-11-17 15:54:49 +0100

    Client: fix linting

--- client/src/components/Game/render.js
+++ client/src/components/Game/render.js
@@ -1,31 +1,31 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
-export default function() {
-  const game = new Phaser.Game({
+export default function () {
+  new Phaser.Game({
     type: Phaser.AUTO,
-    parent: "phaser-container",
+    parent: 'phaser-container',
     width: 800,
     height: 600,
     scene: {
       preload,
-      create
-    }
+      create,
+    },
   });
 
   function preload() {
-    this.load.image("bob", "/assets/images/bob.png");
+    this.load.image('bob', '/assets/images/bob.png');
   }
 
   function create() {
-    const logo = this.add.image(400, 150, "bob");
+    const logo = this.add.image(400, 150, 'bob');
 
     this.tweens.add({
       targets: logo,
       y: 450,
       duration: 2000,
-      ease: "Power2",
+      ease: 'Power2',
       yoyo: true,
-      loop: -1
+      loop: -1,
     });
   }
 }
