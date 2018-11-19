5909266b4776db11c0dc61d38a0b42f0ac3e7d0d
Nicolas Rouvière <nicolasrouviere6@gmail.com>
2018-11-19 02:32:01 +0100

    Client: Add new tiles file

--- client/src/engine/scenes/main/tiles.js
+++ client/src/engine/scenes/main/tiles.js
@@ -2,6 +2,35 @@ const rock = 'R';
 const path = ' ';
 const wall = '#';
 
+// export default function (current, [top, right, bottom, left]) {
+//   switch (current) {
+//     case path:
+//       return 0;
+//     case rock:
+//       return 2;
+//     case wall:
+//       if (bottom === path) {
+//         if (left === path && right === path) return 13;
+//         else if (right === path) return 12;
+//         else if (left === path) return 11;
+//         return 10;
+//       }
+//
+//       if (top === path && right === path && left === path) return 9;
+//       else if (top === path && right === path) return 8;
+//       else if (top === path && left === path) return 7;
+//       else if (top === path) return 5;
+//       else if (right === path && left === path) return 6;
+//       else if (right === path) return 4;
+//       else if (left === path) return 3;
+//
+//       return 2;
+//     default:
+//       return -1;
+//   }
+// }
+
+// eslint-disable-next-line no-unused-vars
 export default function (current, [top, right, bottom, left]) {
   switch (current) {
     case path:
@@ -9,21 +38,7 @@ export default function (current, [top, right, bottom, left]) {
     case rock:
       return 2;
     case wall:
-      if (bottom === path) {
-        if (left === path && right === path) return 13;
-        else if (right === path) return 12;
-        else if (left === path) return 11;
-        return 10;
-      }
-
-      if (top === path && right === path && left === path) return 9;
-      else if (top === path && right === path) return 8;
-      else if (top === path && left === path) return 7;
-      else if (top === path) return 5;
-      else if (right === path && left === path) return 6;
-      else if (right === path) return 4;
-      else if (left === path) return 3;
-
+      if (bottom === path) return 3;
       return 2;
     default:
       return -1;
